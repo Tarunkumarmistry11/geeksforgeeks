80f74a2fea705f7496bb5bdebac4f092ebf27874
TARUN KUMAR MISTRY <132067933+Tarunkumarmistry11@users.noreply.github.com>
2024-05-29 01:01:05 +0530

    Updated Solution - GfG to GitHub

--- Basic/Cyclically rotate an array by one/cyclically-rotate-an-array-by-one.js
+++ Basic/Cyclically rotate an array by one/cyclically-rotate-an-array-by-one.js
@@ -72,11 +72,13 @@ function main() {
 class Solution{
     rotate(arr,n){
         //code here
-        let last_element = arr[n - 1];
-for (let i = n - 1; i > 0; i--) {
-  arr[i] = arr[i - 1];
-}
-arr[0] = last_element;
-return arr;
+        let last_element = arr[n-1];
+        
+        for(let i = n-1; i>0; i--) {
+            arr[i] = arr[i-1];
+        }
+        arr[0] = last_element;
+        return arr;
     }
+
 }
